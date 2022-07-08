3cbd9a4e57d1e3da88fc7bf533582fb72f4b3a22
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-08 18:34:07 +0530

    [Shivaraj] Added Jason Serialization

---
+++ IOoperationLearningProblem/IOoperationLearningProblem/JsonDataOperation.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperation
+{
+    /// <summary>
+    /// Json Input and Output Operations
+    /// </summary>
+    class JasonDataOperaton
+    {
+        /// <summary>
+        /// Jsons the serialize
+        /// </summary>
+        public static void JsonSerialize()
+        {
+            string jsonPath = @"C:\Users\4shiv\OneDrive\Desktop\Fellowship\Assignments\Assignment_Day_28\Files\JsonData.json";
+            //Object Intialiser Instead if adding person.personId = 12
+            //Object Intialiser = We can intialise the value to the fields or property of a class
+            //at the time of creating object without calling a constructor.
+            List<Person> person = new List<Person>()
+
+            {
+               new Person(){PersonId = 12, Name = "Shivaraj", Address = "Delhi"},
+               new Person(){PersonId = 13, Name = "Arun", Address = "Bangalore"},
+               new Person(){PersonId = 12, Name = "Deepak", Address = "Bangal"},
+            };
+            //Serialise Object method used to convert from object into json data
+            string result = JsonConvert.SerializeObject(person);
+            //Writing in to file
+            File.WriteAllText(jsonPath, result);
+        }
+        public static bool IsFileExists(string path)
+        {
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("File Not Exists");
+                return false;
+            }
+        }
+
+    }
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id : " + PersonId + " Name " + Name + "Address" + Address;
+        }
+    }
+}
--- IOoperationLearningProblem/IOoperationLearningProblem/Program.cs
+++ IOoperationLearningProblem/IOoperationLearningProblem/Program.cs
@@ -1,2 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To IO Learning Problems");
+namespace FileIOOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To IO Learning Problems");
+            Console.WriteLine("1 for Jason Serialise");
+            
+
+            int Problem = Convert.ToInt32(Console.ReadLine());
+            switch (Problem)
+            {
+                case 1:
+                    JasonDataOperaton.JsonSerialize();
+                    Console.ReadLine();
+                    break;
+                
+                default:
+                    break;
+            }
+
+        }
+
+    }
+}
+
+//Jason Serialisation
+//In JSON File("C:\Users\4shiv\OneDrive\Desktop\Fellowship\Assignments\Assignment_Day_28\Files\JsonData.json")
+
+//Result:
+//[
+
+//  {
+//    "PersonId": 12,
+//    "Name": "Shivaraj",
+//    "Address": "Delhi"
+//  },
+//  {
+//    "PersonId": 13,
+//    "Name": "Arun",
+//    "Address": "Bangalore"
+//  },
+//  {
+//    "PersonId": 12,
+//    "Name": "Deepak",
+//    "Address": "Bangal"
+//  }
+//]
